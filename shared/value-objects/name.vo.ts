585e19559c67721ce0d3fd55b9d1d3443e75b01a
Ismaylla <bismaylla@gmail.com>
2025-06-02 15:52:14 -0300

    feat: adiciona value objects para name corretamente

--- shared/value-objects/name.vo.ts
+++ shared/value-objects/name.vo.ts
@@ -1,24 +1,53 @@
 export class Name {
-    
+
   private readonly name: string;
 
+  private static readonly MIN_LEN = 2;
+  private static readonly MAX_LEN = 100;
+
   constructor(name: string) {
-    this.validateName(name);
-    this.name = name.trim();
+    const nameNormalized = Name.normalizeName(name);
+    Name.validateName(nameNormalized);
+
+    this.name = nameNormalized;
   }
 
-  private validateName(name: string): boolean {
-    const trimmed = name.trim();
 
-    if (!trimmed) {
+private static normalizeName(name: string): string {
+  return Name.removeEdgeSpaces(name);
+}
+
+
+  private static validateName(name: string): void {
+    Name.validateEmpty(name);
+    Name.validateLength(name);
+    Name.validateCharacters(name);
+  }
+
+
+  private static removeEdgeSpaces(name: string): string {
+    return name.trim();
+  }
+
+  private static validateEmpty(name: string): void {
+    if (!name) {
       throw new Error("O nome não pode estar vazio.");
     }
+  }
 
-    if (trimmed.length < 2 || trimmed.length > 100) {
-      throw new Error("O nome deve ter entre 2 e 100 caracteres.");
+  private static validateLength(name: string): void {
+    if (name.length < Name.MIN_LEN || name.length > Name.MAX_LEN) {
+      throw new Error(
+        `O nome deve ter entre ${Name.MIN_LEN} e ${Name.MAX_LEN} caracteres.`
+      );
     }
+  }
 
-    return true;
+  private static validateCharacters(name: string): void {
+    const validNameRegex = /^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/u;
+    if (!validNameRegex.test(name)) {
+      throw new Error("O nome contém caracteres inválidos.");
+    }
   }
 
   public getName(): string {
