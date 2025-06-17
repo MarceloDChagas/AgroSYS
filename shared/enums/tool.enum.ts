64fb05c573a89295ecbafc5da3c3fe4bee043d83
Ismaylla <bismaylla@gmail.com>
2025-06-17 06:59:00 -0300

    test: Adiciona testes para ToolService

--- shared/enums/tool.enum.ts
+++ shared/enums/tool.enum.ts
@@ -1,9 +1,11 @@
 export enum EStatusTool {
-  REQUESTED = "SOLICITADA",
-  RETURNED = "DEVOLVIDA",
-  LENDING = "EMPRESTADA",
+  REQUESTED = 'SOLICITADA',
+  RETURNED = 'DEVOLVIDA',
+  LENDING = 'EMPRESTADA',
 }
 
 export enum EToolName {
-  
+  ENXADA = 'ENXADA',
+  PA = 'PA',
+  TRATOR = 'TRATOR',
 }
